69c06ba80b3dc67b59ab5bbe130cf740e8bc67d2
Spandita Banerjee <spanditabanerjee@gmail.com>
2021-07-27 20:54:00 +0530

    only logic

---
+++ maximumsubarraysum.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum=0;
+        int maximum=nums[0];
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            if(sum>maximum) maximum=sum;
+            if(sum<0) sum=0;
+        }
+        
+        return maximum;
+        
+    }
+}
