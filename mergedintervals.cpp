a3c40913ef009643700b411b25c63e108c158b04
Spandita Banerjee <spanditabanerjee@gmail.com>
2021-07-27 21:40:51 +0530

    first commit

---
+++ mergedintervals.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        
+        vector<vector<int>> mergedintervals;
+        if(intervals.size()==0)
+        {
+            return mergedintervals;
+        }
+        sort(intervals.begin(),intervals.end());
+        vector<int> temp=intervals[0];
+        
+        for(auto it : intervals){
+            if(it[0] <= temp[1])
+            {
+                temp[1] = max(it[1], temp[1]);
+            }
+            else{
+                mergedintervals.push_back(temp);
+                temp=it;
+            }
+        }
+        mergedintervals.push_back(temp);
+        return mergedintervals;
+        
+        
+    }
+};
