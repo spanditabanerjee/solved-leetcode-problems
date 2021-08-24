3a1cee8e510b21027b22d5349dc5e931f6a8c5a0
Spandita Banerjee <spanditabanerjee@gmail.com>
2021-08-24 23:18:11 +0530

    optimised

--- majorityelement.cpp
+++ majorityelement.cpp
@@ -4,7 +4,7 @@
 
 class Solution {
 public:
-    int majorityEle(vector<int>& nums) {
+    int majorityElement(vector<int>& nums) {
         int count = 0;
         int element = 0;
         for(int num : nums){
