26dda3824a6622d537364e6e8431c720a5800499
Spandita Banerjee <spanditabanerjee@gmail.com>
2021-08-27 23:55:39 +0530

    commit

--- maximumsubarraysum.java
+++ maximumsubarraysum.java
@@ -1,14 +1,14 @@
 class Solution {
     public int maxSubArray(int[] nums) {
         int sum=0;
-        int maxim=nums[0];
+        int maximum=nums[0];
         for(int i=0;i<nums.length;i++){
             sum+=nums[i];
-            if(sum>maxim) maxim=sum;
+            if(sum>maximum) maximum=sum;
             if(sum<0) sum=0;
         }
         
-        return maxim;
+        return maximum;
         
     }
 }
