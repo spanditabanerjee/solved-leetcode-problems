a9330fd1028d65047611e046007aa9c9a9ea0069
Spandita Banerjee <spanditabanerjee@gmail.com>
2021-08-30 18:55:30 +0530

    fixed

--- mergesortedarrays.cpp
+++ mergesortedarrays.cpp
@@ -43,7 +43,7 @@ int main()
     merge(X, Y, m, n);
  
     cout << "X: "; printArray(X, m);
-    cout << "value of Y: "; printArray(Y, n);
+    cout << "Y: "; printArray(Y, n);
  
     return 0;
 }
