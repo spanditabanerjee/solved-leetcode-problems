8c28e67b59f9933e00f17751937457294064f8c2
Spandita Banerjee <spanditabanerjee@gmail.com>
2021-08-31 12:13:59 +0530

    fix

--- majorityelement.cpp
+++ majorityelement.cpp
@@ -8,6 +8,7 @@ public:
         int count = 0;
         int element = 0;
         for(int num : nums){
+            
             if(count == 0)
             {element = num;}
             if(num == element)
