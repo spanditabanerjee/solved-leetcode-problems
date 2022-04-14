862981f3e4cfd169be9ac27369298314b6015c64
Spandita Banerjee <spanditabanerjee@gmail.com>
2022-04-14 18:28:25 +0530

    leetcode day 11

--- SearchinBST.java
+++ SearchinBST.java
@@ -11,5 +11,9 @@ public class TreeNode {
          }
  }
 class SearchinBST{
-    
+    public TreeNode searchBST(TreeNode root, int val) {
+        if(root == null || root.val == val) return root;
+        return val < root.val? searchBST(root.left, val): searchBST(root.right, val);
+        
+    }
 }
