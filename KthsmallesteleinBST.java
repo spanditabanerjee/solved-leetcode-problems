82443a51b2179bc67b247f88e8609cb3145b4075
Spandita Banerjee <spanditabanerjee@gmail.com>
2022-04-18 08:48:46 +0530

    day 15

---
+++ KthsmallesteleinBST.java
@@ -0,0 +1,19 @@
+class KthsmallesteleinBST{
+    public static int ans = 0;
+    public int kthSmallest(TreeNode root, int k) {
+        helper(root, k);
+        return ans;
+    }
+    
+    public int helper(TreeNode root, int k) {
+        if (root == null) {
+            return 0;
+        }
+        int leftCount = helper(root.left, k);
+        int rightCount = helper(root.right, k - leftCount - 1);
+        if (k == leftCount + 1) {
+            ans = root.val;
+        }
+        return leftCount + rightCount + 1;
+    }
+    };
