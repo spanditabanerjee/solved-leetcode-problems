97b4192ce273ac017f88fb80f3efae9b7e67727f
Spandita Banerjee <spanditabanerjee@gmail.com>
2022-05-01 08:37:57 +0530

    day 28

---
+++ backspaceStringCompare.py
@@ -0,0 +1,6 @@
+def backspaceCompare(self, S, T):
+    def back(res, c):
+        if c != '#': res.append(c)
+        elif res: res.pop()
+        return res
+    return reduce(back, S, []) == reduce(back, T, [])
